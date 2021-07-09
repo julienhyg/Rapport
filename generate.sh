ac3844f109789bc1a49e159eff3e20850ef1c22a
Julien <julienhuyghe1@gmail.com>
2021-07-09 09:09:27 +0200

    Premier commit
    Défini la structure, les templates, la configuration (metadata.yaml) ainsi que le script (generate.sh) pour obtenir le rapport (genDocument.md).

--- generate.sh
+++ generate.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-pandoc intro.md metadata.yaml -o genDocument.pdf --template=eisvogel --pdf-engine=xelatex
+pandoc 1+.md 2+.md 3+.md 4+.md metadata.yaml -o genDocument.pdf --template=eisvogel --pdf-engine=xelatex
