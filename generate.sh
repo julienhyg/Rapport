515e3c18d0f60c90d46db1ad9f177e52a351a43d
Julien <32270723+julienhyg@users.noreply.github.com>
2021-07-16 14:50:50 +0200

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -1,2 +1,2 @@
 #!/bin/zsh
-pandoc 1+.md 2+.md 3+.md 4+.md metadata.yaml -o genDocument.pdf --template=eisvogel --pdf-engine=xelatex --css=extra.css
+pandoc 1+.md 2+.md 3+.md 4+.md metadata.yaml -o BACH2A_RapportStage_JulienHUYGHE.pdf --template=eisvogel --pdf-engine=xelatex --css=extra.css
